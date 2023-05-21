dc89d06d299d4b082cbd81b1e5044a12d8fc7d95
Kirill Grenader <k.grenader@yandex.ru>
2023-05-21 17:00:06 +0300

    3 урок не доделано управление перемещением персонажа

--- hero.py
+++ hero.py
@@ -1,3 +1,8 @@
+key_switch_camera = 'c'
+
+key_turn_left = 'n'
+key_turn_right = 'm'
+
 class Hero():
 
     def __init__(self, pos, land):
@@ -9,10 +14,38 @@ class Hero():
         self.hero.setPos(pos)
         self.hero.reparentTo(render)
         self.cameraBind()
+        self.accept_events()
 
     def cameraBind(self):
         base.disableMouse()
         base.camera.setH(180)
-        base.acmera.reparentTo(self.hero)
+        base.camera.reparentTo(self.hero)
         base.camera.setPos(0, 0, 1.5)
         self.cameraOn = True
+
+    def cameraUp(self):
+        pos = self.hero.getPos()
+        base.mouseInterfaceNode.setPos(-pos[0], -pos[1], -pos[2]-3)
+        base.camera.reparentTo(render)
+        base.enableMouse()
+        self.cameraOn = False
+
+    def turn_left(self):
+        self.hero.setH((self.hero.getH() + 5) % 360)
+
+    def turn_right(self):
+        self.hero.setH((self.hero.getH() - 5) % 360)
+
+    def changeView(self):
+        if self.cameraOn:
+            self.cameraUp()
+        else:
+            self.cameraBind()
+
+    def accept_events(self):
+        base.accept(key_turn_left, self.turn_left)
+        base.accept(key_turn_left + '-repeat', self.turn_left)
+        base.accept(key_turn_right, self.turn_right)
+        base.accept(key_turn_right + '-repeat', self.turn_right)
+
+        base.accept(key_switch_camera, self.changeView)
